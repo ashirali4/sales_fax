dc7b0308600354f0cd3cc66263d45723c1b1edf9
ashir.muhammad <itxaxhu668@gmail.com>
2021-06-19 00:36:31 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/fax/salesfax/sales_fax/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fax.salesfax.sales_fax
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/auth/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class Login extends StatefulWidget {
+
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ));
+  }
+}
